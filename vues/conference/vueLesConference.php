2916a0325305a1fae2d448504de9def625dd15b1
nunzimateo <56727504+nunzimateo@users.noreply.github.com>
2019-11-13 09:02:09 +0100

    Update vueLesConference.php

--- vues/conference/vueLesConference.php
+++ vues/conference/vueLesConference.php
@@ -13,7 +13,11 @@ foreach($listeConference as $uneConference){
     <td><?php echo "Conferencier : ".$unConferencier[1]."  ".$unConferencier[2]; ?></td>
     <td><?php echo "<img src=vues/conferencier/image/".$unConferencier[3].">" ?></td>
     <td><?php echo "Description : ".$unConferencier[4]; ?></td>
-   </tr>
+    <td><?php echo "Date : ".$uneConference[6]; ?></td>
+    <?php if(!isset($_SESSION["conf"])&isset($_SESSION["log"])){ ?>
+    <td><?php echo "<a href=index.php?clt=personne&action=inscription&conf=".$uneConference[0]."&date=".$uneConference[6].">S'inscrire</a>"; ?></td>
+    <?php } ?>
+    </tr>
    <?php
 }
 ?>
