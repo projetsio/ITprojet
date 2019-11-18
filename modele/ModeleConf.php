8eafefb50b997604965103a1f17041083a7ea38a
nunzimateo <56727504+nunzimateo@users.noreply.github.com>
2019-11-18 09:59:38 +0100

    Update ModeleConf.php

--- modele/ModeleConf.php
+++ modele/ModeleConf.php
@@ -2,48 +2,97 @@
 require_once  'dbPdo.php';
 
 class ModeleConf extends dbPdo {
-		public static function getLesConf() {
-		try {
-			
+	public static function getLesConf() {
+		try {			
 			$sql="SELECT * FROM conference";
 			$result=dbPdo::$pdo->query($sql);
 			$liste=$result->fetchAll();
 			return $liste;
-			
-			}catch (PDOException $e) {
-				echo $e->getMessage();
-				die("Erreur dans la BDD");
+		
 			}
+		catch (PDOException $e) 
+		{
+			echo $e->getMessage();
+			die("Erreur dans la BDD");
+		}
 	}
 
-	public static function GetConferencierByConference($id)
-	{
-
-	try
-	{
-		$sql="SELECT * from conferencier where id = '$id'";
-		$result = dbPDO::$pdo->query($sql);
-		$liste = $result->fetchAll();
-		return $liste;
+	public static function GetConferencierByConference($id){
+		try{
+			$sql="SELECT * from conferencier where id = '$id'";
+			$result = dbPdo::$pdo->query($sql);
+			$liste = $result->fetchAll();
+			return $liste;
 		
 		}
 		catch (PDOException $e)
 		{
-		echo $e->getMessage();
-		die ("Erreur dans la BDD");
+			echo $e->getMessage();
+			die ("Erreur dans la BDD");
+		}
+	}
+
+	public static function GetSalleByConference($id){
+		try{
+			$sql="SELECT * from salle where id = '$id'";
+			$result = dbPdo::$pdo->query($sql);
+			$liste = $result->fetchAll();
+			return $liste;
+		
+		}
+		catch (PDOException $e)
+		{
+			echo $e->getMessage();
+			die ("Erreur dans la BDD");
 		}
 	}
 	
 	public static function inscription($date,$idc,$ide){
 		try{
-			$sql="INSERT INTO reservation VALUES ($date,$idc,$ide)";
-			$result=dpPDO::$pdo->exec($sql);
+			$sql="INSERT INTO reservation VALUES ('$date','$idc','$ide')";
+			$result=dbPdo::$pdo->exec($sql);
 		}
 		catch (PDOException $e){
 			echo $e->getMessage();
 			die ("Erreur dans la BDD");
 		}
 	}
-		
+
+	public static function testinscription($id){
+		try{
+			$sql="SELECT * FROM reservation where ideleve='$id'";
+			$result= dbPdo::$pdo->query($sql);
+			$liste = $result->fetch();
+			if(empty($liste)){
+				return "vide";
+			}else{
+				return $liste;
+			}
+		}
+		catch (PDOException $e)
+		{
+			echo $e->getMessage();
+			die ("Erreur dans la BDD");
+		}
+	}
+
+	public static function PlacesRestantes($id){
+		try{
+			$sql="SELECT COUNT(ideleve) FROM reservation where idconference='$id'";
+			$result= dbPdo::$pdo->query($sql);
+			$liste = $result->fetch();
+			if(empty($liste)){
+				return "vide";
+			}else{
+				return $liste;
+			}
+		}
+		catch (PDOException $e)
+		{
+			echo $e->getMessage();
+			die ("Erreur dans la BDD");
+		}
+	}
+			
 }
 ?>
