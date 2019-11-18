44ef11184ac59a0e2989f404d514a36322e321a7
nunzimateo <56727504+nunzimateo@users.noreply.github.com>
2019-11-18 10:01:12 +0100

    Update vueLesConference.php

--- vues/conference/vueLesConference.php
+++ vues/conference/vueLesConference.php
@@ -1,28 +1,34 @@
 <table>
 <?php
-foreach($listeConference as $uneConference){
-?>
+foreach($listeConference as $uneConference){ ?>
   <tr>
+    <?php $Conferencier = ModeleConf::GetConferencierByConference($uneConference[4]); 
+    $Salle = ModeleConf::GetSalleByConference($uneConference[2]);
+    $place = ModeleConf::PlacesRestantes($uneConference[0]);
+     ?>
     <td><?php echo "Titre de la conference : ".$uneConference[1] ?></td>
-    <td><?php echo "Salle : ".$uneConference[2]; ?></td>
-    <td><?php echo "Theme : ".$uneConference[4]; ?></td>
-    <?php $Conferencier = ModeleConf::GetConferencierByConference($uneConference[5]); 
-    	foreach($Conferencier as $unConferencier)
-    	{
-    ?>
-    <td><?php echo "Conferencier : ".$unConferencier[1]."  ".$unConferencier[2]; ?></td>
-    <td><?php echo "<img src=vues/conferencier/image/".$unConferencier[3].">" ?></td>
-    <td><?php echo "Description : ".$unConferencier[4]; ?></td>
-    <td><?php echo "Date : ".$uneConference[6]; ?></td>
-    <?php if(!isset($_SESSION["conferencier"])&isset($_SESSION["log"])){ ?>
-    <td><?php echo "<a href=index.php?clt=personne&action=inscription&conf=".$uneConference[0]."&date=".$uneConference[6].">S'inscrire</a>"; ?></td>
-    <?php } ?>
+    <td><?php echo "Theme : ".$uneConference[3]; ?></td>
+    <?php foreach($Salle as $uneSalle){ 
+    $reste = intval($uneSalle[2]) - intval($place[0]); ?>
+    <td><?php echo "Salle : ".$uneSalle[1]; ?></td>
+    <td><?php echo "Places restantes : ".$reste; ?></td>
+    <?php
+    }foreach($Conferencier as $unConferencier){ ?>
+        <td><?php echo "Conferencier : ".$unConferencier[1]."  ".$unConferencier[2]; ?></td>
+        <td><?php echo "<img src=vues/conferencier/image/".$unConferencier[3].">" ?></td>
+        <td><?php echo "Description : ".$unConferencier[4]; ?></td>
+        <td><?php echo "Date : ".$uneConference[5]; ?></td>
+        <?php if(!isset($_SESSION["conferencier"])&isset($_SESSION["log"])){ 
+            $test=ModeleConf::testinscription($_SESSION['id']);
+            if ($test=="vide"){ 
+                echo "<td><a href=index.php?clt=conf&action=inscription&conf=".$uneConference[0]."&date=".$uneConference[5]."&id=".$_SESSION['id'].">S'inscrire</a></td>"; 
+            }else{
+                echo "<td>Déjà inscrit</td>";
+            } 
+        } ?>
     </tr>
    <?php
-}
-?>
+    } ?>
 <?php
-}
-?>
+} ?>
 </table>
-
