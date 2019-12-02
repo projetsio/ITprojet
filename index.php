be896390c2e4e4fa4cd23b29ad6ca15b08741ddd
nunzimateo <56727504+nunzimateo@users.noreply.github.com>
2019-12-02 09:32:39 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,4 +3,27 @@ session_start();
 include("vues/entete.php");
 include("vues/zonePle.php");
 include("vues/piedPage.php");
+
+if(isset($_GET['ctl']))
+{
+	$control = $_GET['ctl'];
+	switch($control)
+	{
+		case 'personne':{
+			include "controleur/ctlPersonne.php";
+			break;
+		}
+
+		case 'conf':{
+			include "controleur/ctlConf.php";
+			break;
+		}
+
+		case 'admin':{
+			include "controleur/ctlAdmin.php";
+		}
+
+	}	
+}
+
 ?>
